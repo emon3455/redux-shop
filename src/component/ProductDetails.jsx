f6a4ce7c9927db31ef29546565d4b621b6be4d55
Emon <emon.mhk69@gmail.com>
2023-08-16 00:47:04 +0600

    product display done

--- src/component/ProductDetails.jsx
+++ src/component/ProductDetails.jsx
@@ -1,14 +1,62 @@
+/* eslint-disable no-unused-vars */
+import * as React from 'react';
 import { useLoaderData } from "react-router-dom";
-
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import { Button } from '@mui/material';
 
 const ProductDetails = () => {
 
     const product = useLoaderData();
-    console.log(product);
+
+    const handleClick = () => {
+        console.log("clicked");
+    }
 
     return (
-        <div>
-            product id {product.id}
+        <div className='max-w-4xl mx-auto my-10'>
+            <Card className='md:flex justify-between'>
+                <div className="w-full md:w-2/5 md:order-2">
+                    <CardMedia
+                        component="img"
+                        image={product.image}
+                        alt="product image"
+                        className='w-full object-contain'
+                    />
+                </div>
+                <Box className="w-full md:w-3/5 md:order-1  flex flex-col justify-center p-2">
+                    <Typography variant="h6" component="div">
+                        {product?.title}
+                    </Typography>
+                    <Typography variant="p" component="div">
+                        <span className='font-semibold'>Category:</span> {product?.category}
+                    </Typography>
+                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                        <span className='font-semibold'>Description:</span> {product?.description}
+                    </Typography>
+                    <div className="flex items-center space-x-2">
+                        <span className='font-semibold'>Rating:</span>
+                        <span className='text-orange-400'>
+                            <StarRateIcon />
+                            <StarRateIcon />
+                            <StarRateIcon />
+                            <StarRateIcon />
+                            <StarRateIcon />
+                        </span>
+                        <span>
+                            {product?.rating?.rate}
+                        </span>
+                    </div>
+
+                    <div className="mt-2">
+                        <Button onClick={handleClick} variant="contained">Add To Cart</Button>
+                    </div>
+                </Box>
+            </Card>
         </div>
     );
 };
